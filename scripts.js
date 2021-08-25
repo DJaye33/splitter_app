5fb034318bc6c359735bfeb23c0c6782441b6f18
Danny JeanLouis <dlouis3386@gmail.com>
2021-08-25 10:11:23 -0400

    fixed app jumping problem when adding border to inputs

---
+++ scripts.js
@@ -0,0 +1,11 @@
+const billInput = document.querySelector(".bill-input");
+const customInput = document.querySelector(".custom");
+const peopleInput = document.querySelector(".people-input");
+const percentBtn = document.querySelectorAll(".percent-btn");
+
+// Capture value of billInput
+// multiply percentage selection to billInput value *active button state*
+// divide result by number of people
+// then gather total
+// disaply in respective locations
+// create reset function
