ea3837e9004ef115f8c4722ec8943d8d5a5e512a
Danny JeanLouis <dlouis3386@gmail.com>
2021-08-26 23:01:00 -0400

    work on js logic

--- scripts.js
+++ scripts.js
@@ -1,11 +1,28 @@
 const billInput = document.querySelector(".bill-input");
-const customInput = document.querySelector(".custom");
-const peopleInput = document.querySelector(".people-input");
-const percentBtn = document.querySelectorAll(".percent-btn");
+const tipPercent = document.querySelectorAll(".percent-btn");
+const nPeople = document.querySelector(".people-input");
+const tipAmount = document.querySelector(".amount");
+const custom = document.querySelector(".custom");
 
-// Capture value of billInput
-// multiply percentage selection to billInput value *active button state*
-// divide result by number of people
-// then gather total
-// disaply in respective locations
-// create reset function
+let bill;
+let tip;
+let numPeople;
+
+billInput.addEventListener("input", () => {
+  bill = +billInput.value;
+  console.log(bill);
+});
+
+tipPercent.forEach((percent) => {
+  percent.addEventListener("click", (event) => {
+    tip = +parseInt(event.currentTarget.textContent, 10);
+    document.querySelector(".percent-btn.active")?.classList.remove("active");
+    event.currentTarget.classList.toggle("active");
+    console.log(tip);
+  });
+});
+
+nPeople.addEventListener("input", () => {
+  numPeople = +nPeople.value;
+  console.log(numPeople);
+});
